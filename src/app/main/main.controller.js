46feab28a01ec063f4dfabac2a42f36c25702c65
KaiKeima <keimakun2011@gmail.com>
2015-06-11 12:00:43 +0900

    いろいろ追加

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,65 +1,24 @@
 'use strict';
 
-angular.module('angularMosimosi')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      {
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
-      },
-      {
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
-      },
-      {
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
-      },
-      {
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
-      },
-      {
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'title': 'Angular Material Design',
-        'url': 'https://material.angularjs.org/#/',
-        'description': 'The Angular reference implementation of the Google\'s Material Design specification.',
-        'logo': 'angular-material.png'
-      },
-      {
-        'title': 'Stylus',
-        'url': 'http://learnboost.github.io/stylus/',
-        'description': 'Stylus is a revolutionary new language, providing an efficient, dynamic, and expressive way to generate CSS. Supporting both an indented syntax and regular CSS style.',
-        'logo': 'stylus.png'
-      },
-      {
-        'key': 'jade',
-        'title': 'Jade',
-        'url': 'http://jade-lang.com/',
-        'description': 'Jade is a high performance template engine heavily influenced by Haml and implemented with JavaScript for node.',
-        'logo': 'jade.png'
-      }
-    ];
-    angular.forEach($scope.awesomeThings, function(awesomeThing) {
-      awesomeThing.rank = Math.random();
+angular.module('angularMosimosi').controller('MainCtrl', function ($scope, $interval) {
+
+  $scope.message = '髪を切る'
+
+  $scope.tasks = [];
+
+  $scope.addOnClick = function(event) {
+    $scope.tasks.push( {
+      'message': $scope.message,
+      'x': event.offsetX-14,
+      'y': event.offsetY-16
     });
-  });
+  };
+
+  var displayTime = function () {
+    $scope.time = new Date();
+  };
+
+  displayTime = $interval(displayTime, 1000);
+
+
+});
